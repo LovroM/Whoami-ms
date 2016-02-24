5c2ad46633203d685ad4a274bdab391c09b59e85
Lovro <mekis.lovro@gmail.com>
2016-02-24 09:03:27 +0000

    ip part works 1/3

--- server.js
+++ server.js
@@ -2,38 +2,32 @@ var express = require('express');
 var app = express();
 var path = require("path");
 
-app.get('/', function(req, res) {
-    res.sendfile(path.join(__dirname, '/client', '/index.html'));
-});
 
-app.get("/:id", function(req, res) {
+app.get("/", function(req, res) {
     
-    var id = String(req.params.id);
-    var responseObj = {
-        "unix": null,
-        "natural": null
-    };
-    var monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
-
-    if (/[0-9]{10}/.test(id)) {
-        responseObj.unix = Number(id);
-
-        var date = new Date(id * 1000);
-        responseObj.natural = monthNames[date.getMonth()] + " " + date.getDay() + ", " + date.getFullYear();
-
-    }
-    else if (/(\w+\s\d+\,\s\d{4})/.test(id)) {
-
-        var date = new Date(id);
-        responseObj.unix = date.getTime()/1000;
-        responseObj.natural = id;
-
-    }
-
-    console.log("Request", id);
-
+    //{"ipaddress":"62.42.167.183","language":"sl-SI","software":"Windows NT 10.0; Win64; x64"}
+    var responseObj = {"ipaddress": null,"language": null,"software": null};
+    
+    /************************** IP **************************/
+    var userIp = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+    
+    responseObj.ipaddress = userIp;
+    
+    /*********************** LANGUAGE ***********************/
+    var userLang = null;
+    
+    
+    responseObj.language = userLang;
+    
+    /*********************** SOFTWARE ***********************/
+    var userSoft = null;
+    
+    
+    responseObj.software = userSoft;
+    
     res.send(responseObj);
 });
 
